ff43eaed71dedc426887a9134318091b96805174
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-11-03 22:10:26 -0400

    updated utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,5 @@
-const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
+module.exports = {
+  format_date: (date) => {
+    return date. toLocalDateString();
   }
-  
 };
-
-module.exports = withAuth
